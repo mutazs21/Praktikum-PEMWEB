144808ed7053254301dcc39d4d85bc2b4976b578
mutazs <140478630+mutazs21@users.noreply.github.com>
2026-05-14 22:08:44 +0700

    Add files via upload

---
+++ storage/framework/views/2398141cca043482a405179037e477f2.php
@@ -0,0 +1,8 @@
+<ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Links</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/praktikum/pemweb1/P9/Laravel/resources/views/layouts/sidebar.blade.php ENDPATH**/ ?>
